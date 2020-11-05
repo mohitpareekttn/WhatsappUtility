e8dc7bf34ebf8ba2ac2b54e8303af4bba047d169
Mohit Pareek <mohit.pareek@tothenew.com>
2020-11-05 17:00:04 +0530

    first commit

--- Whatsapp utility demo/Whatsapp utility demo/ViewController.swift
+++ Whatsapp utility demo/Whatsapp utility demo/ViewController.swift
@@ -15,6 +15,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        
+        let phoneNumber =  "8259885915" // you need to change this number
+        let appURL = URL(string: "https://api.whatsapp.com/send?phone=\(phoneNumber)")!
+        if UIApplication.shared.canOpenURL(appURL) {
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(appURL, options: [:], completionHandler: nil)
+            }
+            else {
+                UIApplication.shared.openURL(appURL)
+            }
+        } else {
+            // WhatsApp is not installed
+        }
+        
+    }
+    
 }
 
