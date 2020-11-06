8040c8b357ef2312af8998e562b48cea32790d40
Mohit Pareek <mohit.pareek@tothenew.com>
2020-11-06 12:25:36 +0530

    after editing whatsapp url function

--- Whatsapp utility demo/Whatsapp utility demo/ViewController.swift
+++ Whatsapp utility demo/Whatsapp utility demo/ViewController.swift
@@ -17,20 +17,50 @@ class ViewController: UIViewController {
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         
+        openWhatsapp()
+//        let phoneNumber =  "8259885915" // you need to change this number
+//        let appURL = URL(string: "https://api.whatsapp.com/send?phone=\(phoneNumber)")!
+//        if UIApplication.shared.canOpenURL(appURL) {
+//            if #available(iOS 10.0, *) {
+//                UIApplication.shared.open(appURL, options: [:], completionHandler: nil)
+//            }
+//            else {
+//                UIApplication.shared.openURL(appURL)
+//            }
+//        } else {
+//            // WhatsApp is not installed
+//        }
+//
+//        let urlWhats = "whatsapp://send?phone=+919789384445&abid=12354&text=Hello"
+//        if let urlString = urlWhats.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed) {
+//            if let whatsappURL = URL(string: urlString) {
+//                if UIApplication.shared.canOpenURL(whatsappURL) {
+//                    UIApplication.shared.openURL(whatsappURL)
+//                } else {
+//                    print("Install Whatsapp")
+//                }
+//            }
+//        }
         
-        let phoneNumber =  "8259885915" // you need to change this number
-        let appURL = URL(string: "https://api.whatsapp.com/send?phone=\(phoneNumber)")!
-        if UIApplication.shared.canOpenURL(appURL) {
-            if #available(iOS 10.0, *) {
-                UIApplication.shared.open(appURL, options: [:], completionHandler: nil)
+    }
+    
+    
+    func openWhatsapp(){
+        let urlWhats = "whatsapp://send?phone=+918259885915&abid=12354&text=Hello"
+        if let urlString = urlWhats.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed){
+            if let whatsappURL = URL(string: urlString) {
+                if UIApplication.shared.canOpenURL(whatsappURL){
+                    if #available(iOS 10.0, *) {
+                        UIApplication.shared.open(whatsappURL, options: [:], completionHandler: nil)
+                    } else {
+                        UIApplication.shared.openURL(whatsappURL)
+                    }
+                }
+                else {
+                    print("Install Whatsapp")
+                }
             }
-            else {
-                UIApplication.shared.openURL(appURL)
-            }
-        } else {
-            // WhatsApp is not installed
         }
-        
     }
     
 }
